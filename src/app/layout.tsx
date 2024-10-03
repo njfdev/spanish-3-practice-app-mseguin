b374b91b1b1cf97f983f5cbc2d5ca4add4f65630
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-10-03 19:51:15 -0400

    Address Build Error Preventing Build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/ModeToggle";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = localFont({
